cdbf253d3ec3cebe98ec8698abaa89822f3f980b
Xamilio <119781901+Xamilio@users.noreply.github.com>
2024-12-01 14:07:20 +0200

    Update ConsoleApplication11.cpp

--- ConsoleApplication11/ConsoleApplication11.cpp
+++ ConsoleApplication11/ConsoleApplication11.cpp
@@ -9,11 +9,11 @@ int main() {
         array[i] = rand() % 61 - 30;
     }
 
-    int array_stupid[SIZE], array_gnome[SIZE], array_bubble[SIZE];
+    int stupid[SIZE], gnome[SIZE], bubble[SIZE];
     for (int i = 0; i < SIZE; ++i) {
-        array_stupid[i] = array[i];
-        array_gnome[i] = array[i];
-        array_bubble[i] = array[i];
+        stupid[i] = array[i];
+        gnome[i] = array[i];
+        bubble[i] = array[i];
     }
 
     int iterations_stupid = 0;
@@ -22,8 +22,8 @@ int main() {
         sorted = true;
         for (int i = 0; i < SIZE - 1; ++i) {
             iterations_stupid++;
-            if (array_stupid[i] > array_stupid[i + 1]) {
-                swap(array_stupid[i], array_stupid[i + 1]);
+            if (stupid[i] > stupid[i + 1]) {
+                swap(stupid[i], stupid[i + 1]);
                 sorted = false;
             }
         }
@@ -33,10 +33,10 @@ int main() {
     int index = 0;
     while (index < SIZE) {
         iterations_gnome++;
-        if (index == 0 || array_gnome[index] >= array_gnome[index - 1]) {
+        if (index == 0 || gnome[index] >= gnome[index - 1]) {
             index++;
         } else {
-            swap(array_gnome[index], array_gnome[index - 1]);
+            swap(gnome[index], gnome[index - 1]);
             index--;
         }
     }
@@ -45,8 +45,8 @@ int main() {
     for (int i = 0; i < SIZE - 1; ++i) {
         for (int j = 0; j < SIZE - i - 1; ++j) {
             iterations_bubble++;
-            if (array_bubble[j] > array_bubble[j + 1]) {
-                swap(array_bubble[j], array_bubble[j + 1]);
+            if (bubble[j] > bubble[j + 1]) {
+                swap(bubble[j], bubble[j + 1]);
             }
         }
     }
