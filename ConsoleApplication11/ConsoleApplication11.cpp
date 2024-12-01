1cebda471d1c9eb182324530df4fcd1f84e8272b
Xamilio <119781901+Xamilio@users.noreply.github.com>
2024-12-01 16:13:45 +0200

    Update ConsoleApplication11.cpp

--- ConsoleApplication11/ConsoleApplication11.cpp
+++ ConsoleApplication11/ConsoleApplication11.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-
 using namespace std;
 
 int main() {
@@ -7,54 +6,66 @@ int main() {
     int array[SIZE];
     int max = 30;
     int min = -30;
-    for (int i = 0; i < SIZE; ++i) {
+    for (int i = 0; i < SIZE; ++i) 
+    {
         array[i] = rand() % (max - min) + min;
     }
 
     int stupid[SIZE], gnome[SIZE], bubble[SIZE];
-    for (int i = 0; i < SIZE; ++i) {
+    for (int i = 0; i < SIZE; ++i) 
+    {
         stupid[i] = array[i];
         gnome[i] = array[i];
         bubble[i] = array[i];
     }
 
-    int iterations_stupid = 0;
+    int itr_stupid = 0;
     bool sorted = false;
-    while (!sorted) {
+    while (!sorted) 
+    {
         sorted = true;
-        for (int i = 0; i < SIZE - 1; ++i) {
-            iterations_stupid++;
-            if (stupid[i] > stupid[i + 1]) {
+        for (int i = 0; i < SIZE - 1; ++i) 
+        {
+            itr_stupid++;
+            if (stupid[i] > stupid[i + 1]) 
+            {
                 swap(stupid[i], stupid[i + 1]);
                 sorted = false;
             }
         }
     }
 
-    int iterations_gnome = 0;
+    int itr_gnome = 0;
     int index = 0;
-    while (index < SIZE) {
-        iterations_gnome++;
-        if (index == 0 || gnome[index] >= gnome[index - 1]) {
+    while (index < SIZE) 
+    {
+        itr_gnome++;
+        if (index == 0 || gnome[index] >= gnome[index - 1]) 
+        {
             index++;
-        } else {
+        }
+        else 
+        {
             swap(gnome[index], gnome[index - 1]);
             index--;
         }
     }
 
-    int iterations_bubble = 0;
-    for (int i = 0; i < SIZE - 1; ++i) {
-        for (int j = 0; j < SIZE - i - 1; ++j) {
-            iterations_bubble++;
-            if (bubble[j] > bubble[j + 1]) {
+    int itr_bubble = 0;
+    for (int i = 0; i < SIZE - 1; ++i) 
+    {
+        for (int j = 0; j < SIZE - i - 1; ++j) 
+        {
+            itr_bubble++;
+            if (bubble[j] > bubble[j + 1]) 
+            {
                 swap(bubble[j], bubble[j + 1]);
             }
         }
     }
 
-    cout << "Stupid Sort iterations: " << iterations_stupid << endl;
-    cout << "Gnome Sort iterations: " << iterations_gnome << endl;
-    cout << "Bubble Sort iterations: " << iterations_bubble << endl;
+    cout << "Stupid Sort iterations: " << itr_stupid << endl;
+    cout << "Gnome Sort iterations: " << itr_gnome << endl;
+    cout << "Bubble Sort iterations: " << itr_bubble << endl;
 
 }
